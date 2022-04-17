caa1cc7c0ab77af9ade481efd57ec48cc964ccc0
B-Eulalia <101184278+B-Eulalia@users.noreply.github.com>
2022-04-17 16:45:27 +0300

    Create tictactoe.cs

---
+++ tictactoe.cs
@@ -0,0 +1,15 @@
+body {
+background-color: bisque;
+}
+
+th, td {
+border: 4px solid;
+border-radius: 5px;
+color: black;
+background-color: lavenderblush;
+text-transform: uppercase;
+}
+
+h1 {
+color: brown;
+}
